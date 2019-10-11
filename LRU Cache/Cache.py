2722feafdeeed6fc296824d6bafba0c86af491ea
Nifbear <43254080+Nifbear@users.noreply.github.com>
2019-10-10 21:15:14 -0400

    Update Cache.py

--- LRU Cache/Cache.py
+++ LRU Cache/Cache.py
@@ -1,3 +1,25 @@
 class Node:
 
     def __init__(self, results):
+        self.results = results
+        self.prev = None
+        self.next = None
+ 
+
+class LinkedList:
+    
+    def __init__(self):
+        self.head = None
+        self.tail = None
+       
+    def move_to_front(self, node): pass # ...
+    def append_to_front(self, node): pass # ...
+    def remove_from_tail(self): pass # ...
+    
+
+class Cache(object):
+    
+    def __init__(self, MAX_SIZE):
+        self.MAX_SIZE = MAX_SIZE
+        self.size = 0
+        self.lookup = {} #
