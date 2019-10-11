418f4983edc3a1e5e667ae61893532e9b636e423
Nifbear <43254080+Nifbear@users.noreply.github.com>
2019-10-10 21:22:41 -0400

    Update Cache.py

--- LRU Cache/Cache.py
+++ LRU Cache/Cache.py
@@ -1,6 +1,7 @@
 class Node:
 
-    def __init__(self, results):
+    def __init__(self, query, results):
+        self.query = query
         self.results = results
         self.prev = None
         self.next = None
@@ -22,4 +23,27 @@ class Cache(object):
     def __init__(self, MAX_SIZE):
         self.MAX_SIZE = MAX_SIZE
         self.size = 0
-        self.lookup = {} #
+        self.lookup = {} # key: query, value: node
+        self.linked_list = LinkedList()
+    
+    def get(self, query):    
+        node = self.lookup.get(query)
+        if node is None:
+            return None
+        self.linked_list.move_to_front(node)
+        return node.results
+    
+    def set(self, results, query):
+        node = self.lookup.get(query)
+        if node is not None:
+            node.results = results
+            self.linked_list.move_to_front(node)
+        else:
+            if self.size == self.MAX_SIZE:
+                self.lookup.pop(self.linked_list.tail.query, None)
+                self.linked_list.remove_from_tail()
+            else:
+                self.size += 1
+            new_node = Node(query, results)
+            self.linked_list.append_to_front(new_node)
+            self.lookup[query] = new_node
